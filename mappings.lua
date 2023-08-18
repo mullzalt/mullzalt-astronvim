b2bb7a40cbab6a48a922a83af9f0abef2f723e67
rizkidmulya <rizki.dimulya@upi.edu>
2023-08-18 16:51:32 +0700

    personalize

--- mappings.lua
+++ mappings.lua
@@ -9,14 +9,16 @@ return {
     -- second key is the lefthand side of the map
 
     -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+    ["<Tab>"] = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+    ["<S-Tab>"] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
+    ["<leader>x"] = { function() require("astronvim.utils.buffer").close() end, desc = "Close buffer" },
+    ["<leader>X"] = { function() require("astronvim.utils.buffer").close(0, true) end, desc = "Force close buffer" },
 
     -- mappings seen under group name "Buffer"
     ["<leader>bD"] = {
@@ -32,6 +34,28 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    ["<C-a>"] = {
+      "gg<S-v>G",
+      desc = "Select All",
+    },
+
+    -- NeoTree
+    ["<C-n>"] = {
+      "<cmd>Neotree toggle<cr>",
+      desc = "Toggle Explorer",
+    },
+    ["<leader>e"] = {
+      function()
+        if vim.bo.filetype == "neo-tree" then
+          vim.cmd.wincmd "p"
+        else
+          vim.cmd.Neotree "focus"
+        end
+      end,
+      desc = "Toggle Explorer Focus",
+    },
+    ["<leader>o"] = false,
   },
   t = {
     -- setting a mapping to false will disable it
